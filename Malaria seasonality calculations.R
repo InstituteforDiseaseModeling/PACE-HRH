0bbceb605e4b34e5f4313701f34f1720545ebdc4
Brittany Hagedorn <bhagedorn@idmod.org>
2021-11-15 16:30:58 -0800

    Minor updates

--- Malaria seasonality calculations.R
+++ Malaria seasonality calculations.R
@@ -16,11 +16,15 @@ dsm1$Month <- as.factor(dsm1$Month)
 region <- read_excel("Malaria seasonality rates.xlsx",sheet="Sheet3")
 dsm1$region <- region$Region[match(dsm1$variable,region$Geography)]
 
+monthtable <- data.frame(month=c("Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"),
+                         monthnum=seq(1,12,1))
+
 lmout <- lm(value ~ Month + variable,data=dsm1)
 lmout <- lm(value ~ Month + region,data=dsm1)
 
 ds2 <- read_excel("Malaria seasonality rates.xlsx",sheet="Sheet2")
 dsm2 <- melt(ds2)
+dsm2$MonthNum <- monthtable$monthnum[match(dsm2$Month,monthtable$month)]
 dsm2$Month <- as.factor(dsm2$Month)
 dsm2$region <- region$Region[match(dsm2$variable,region$Geography)]
 
@@ -28,3 +32,14 @@ lmout <- lm(value ~ Month + region,data=dsm2) #R2 .52
 lmout <- lm(value ~ Month*region,data=dsm2) #R2 .55
 
 #lmerout <- lmer(value ~ (1|region) + (1|Month),data=dsm2) #singular fit
+
+#lmerout <- lmer(value ~ (Month|region),data=dsm2) #singular fit
+
+mspl <- smooth.spline(dsm2$Month,dsm2$value)
+
+## Residual (Tukey Anscombe) plot:
+plot(residuals(mspl) ~ fitted(mspl))
+abline(h = 0, col = "gray")
+
+## The chosen inner knots in original x-scale :
+with(mspl$fit, min + range * knot[-c(1:3, nk+1 +1:3)]) # == unique(cars$speed)
