21ef466c159efe92433fb17b3501d929a7229490
Charles Eliot <charles.eliot@gatesfoundation.org>
2022-01-03 14:55:00 -0800

    First stab at the code the moves parameters between the base, epsilon, and experiment environments

--- ehep/R/ehep_debug_utils.R
+++ ehep/R/ehep_debug_utils.R
@@ -2,8 +2,12 @@
 #'
 #' @return
 dispEnvs <- function(){
+  print("----- GLOBAL -----")
   print(ls.str(globalPackageEnvironment))
+  print("------ BASE ------")
   print(ls.str(baseValuesEnvironment))
+  print("----- EPSILON ----")
   print(ls.str(epsilonValuesEnvironment))
+  print("--- EXPERIMENT ---")
   print(ls.str(experimentValuesEnvironment))
 }
--- ehep/R/ehep_exp_control.R
+++ ehep/R/ehep_exp_control.R
@@ -1,23 +1,48 @@
 # The functions in this module control how EHEP handles stochastic experiments.
 
 SaveBaseSettings <- function(){
-  baseValuesEnvironment$fertilityRates <-
-    ifelse(exists("fertilityRates", where = globalPackageEnvironment),
-           globalPackageEnvironment$fertilityRates, NULL)
+  # baseValuesEnvironment$fertilityRates <-
+  #   ifelse(exists("fertilityRates", where = globalPackageEnvironment),
+  #          globalPackageEnvironment$fertilityRates, NULL)
+  #
+  # baseValuesEnvironment$mortalityRates <-
+  #   ifelse(exists("mortalityRates", where = globalPackageEnvironment),
+  #          globalPackageEnvironment$mortalityRates, NULL)
+  #
+  # baseValuesEnvironment$initialPopulation <-
+  #   ifelse(exists("initialPopulation", where = globalPackageEnvironment),
+  #          globalPackageEnvironment$initialPopulation, NULL)
+  #
+  # baseValuesEnvironment$initialPopulation <-
+  #   ifelse(exists("initialPopulation", where = globalPackageEnvironment),
+  #          globalPackageEnvironment$initialPopulation, NULL)
+  #
 
-  baseValuesEnvironment$mortalityRates <-
-    ifelse(exists("mortalityRates", where = globalPackageEnvironment),
-           globalPackageEnvironment$mortalityRates, NULL)
+  envs <- globalPackageEnvironment
+  envd <- baseValuesEnvironment
 
-  baseValuesEnvironment$initialPopulation <-
-    ifelse(exists("initialPopulation", where = globalPackageEnvironment),
-           globalPackageEnvironment$initialPopulation, NULL)
+  if (exists("populationChangeParameters", where = envs)){
+    envd$populationChangeParameters <- envs$populationChangeParameters
+  } else {
+    envd$populationChangeParameters <- NULL
+  }
+}
 
-  baseValuesEnvironment$initialPopulation <-
-    ifelse(exists("initialPopulation", where = globalPackageEnvironment),
-           globalPackageEnvironment$initialPopulation, NULL)
+ZeroEpsilons <- function(){
+  # Create default PopulationChangeParameters object
+  pcp <- list(initValues = PopulationChangeParameters(),
+              changeRates = PopulationChangeParameters())
+  # Write into the 'epsilon' environment
+  epsilonValuesEnvironment$populationChangeParameters <- pcp
+}
 
-  baseValuesEnvironment$populationChangeParameters <-
-    ifelse(exists("populationChangeParameters", where = globalPackageEnvironment),
-           globalPackageEnvironment$populationChangeParameters, NULL)
+SetExperimentValues <- function(){
+  # TODO: Insert check that all the needed values exist
+
+  baseVar <- baseValuesEnvironment$populationChangeParameters
+  epsilonVar <- epsilonValuesEnvironment$populationChangeParameters
+  pcp = list(initValues = add(baseVar$initValues, epsilonVar$initValues),
+             changeRates = add(baseVar$changeRates, epsilonVar$changeRates))
+
+  experimentValuesEnvironment$populationChangeParameters <- pcp
 }
