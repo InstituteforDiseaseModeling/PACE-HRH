b33ae59d568c1baf9a82b0240010bcd0eb4c0d7c
Brittany Hagedorn <bhagedorn@idmod.org>
2022-02-07 13:59:19 -0800

    Population calibration final analysis set

--- Population calibration analysis.R
+++ Population calibration analysis.R
@@ -11,88 +11,172 @@ library(ggrepel)
 library(scales)
 library(treemapify)
 
+date <- "Feb7"
+scenario <- "PopTest_NtlTotal"
+
 
 UNpop <- read_excel("PopProjections_UN.xlsx",sheet="Sheet1")
 UNpop <- melt(UNpop,variable.name = "Year",value.name = "PopEstimate")
 UNpop$Year <- as.numeric(as.character(UNpop$Year))
 
-scenario <- "PopCheck_NationalTotal"
-append <- "_2_1_15_1_2" #fertility p, mortality p, delta fertility p, delta mortality p, delta q
-calib1 <- read.csv(paste("results/results_",scenario,"_",append,".csv",sep=""))
-#Calculate modeled 0-4 pop to match UN estimates.
-#append_temp <- ddply(subset(calib1,Task_ID=="PopCheck_1t4" | Task_ID=="PopCheck_births"),.(Scenario_ID,Run_num,Trial_num,Year,Month),summarize,Num_services=sum(Num_services),Task_ID="PopCheck_0t4",Service_time=0,Health_benefit=NA)
-#calib1 <- rbind(calib1,append_temp)
-#Summarize calibration runs
-simstats1 <- ddply(calib1,.(Task_ID,Run_num,Trial_num,Year),summarize,popvalue=sum(Num_services))
-simCI1 <- ddply(simstats1,.(Task_ID,Year),summarize,CI20=quantile(popvalue,probs=c(.20)),CI80=quantile(popvalue,probs=c(.80)),CI50 = quantile(popvalue,probs=c(.50)),CI95 = quantile(popvalue,probs=c(.95)),CI05 = quantile(popvalue,probs=c(.05)))
-simCImelt1 <- melt(simCI1,id.vars=c("Task_ID","Year"),variable.name = "StatType", value.name="PopEstimate")
+make_plots <- function(scenarioname,appendtext){
 
-scenario <- "PopCheck_NationalTotal"
-append <- "_2_1_15_1_125" #fertility p, mortality p, delta fertility p, delta mortality p, delta q
-calib2 <- read.csv(paste("results/results_",scenario,"_",append,".csv",sep=""))
-#Calculate modeled 0-4 pop to match UN estimates.
-#append_temp <- ddply(subset(calib2,Task_ID=="PopCheck_1t4" | Task_ID=="PopCheck_births"),.(Scenario_ID,Run_num,Trial_num,Year,Month),summarize,Num_services=sum(Num_services),Task_ID="PopCheck_0t4",Service_time=0,Health_benefit=NA)
-#calib2 <- rbind(calib2,append_temp)
-#Summarize calibration runs
-simstats2 <- ddply(calib2,.(Task_ID,Run_num,Trial_num,Year),summarize,popvalue=sum(Num_services))
-simCI2 <- ddply(simstats2,.(Task_ID,Year),summarize,CI20=quantile(popvalue,probs=c(.20)),CI80=quantile(popvalue,probs=c(.80)),CI50 = quantile(popvalue,probs=c(.50)),CI95 = quantile(popvalue,probs=c(.95)),CI05 = quantile(popvalue,probs=c(.05)))
-simCImelt2 <- melt(simCI2,id.vars=c("Task_ID","Year"),variable.name = "StatType", value.name="PopEstimate")
+  calib <- read.csv(paste("results/results_",scenarioname,"_",date,"_",appendtext,".csv",sep=""))
+  
+  
+  ######################
+  ### Format data sets
+  
+  #Summarize calibration runs
+  simstats <- ddply(calib,.(Task_ID,Run_num,Trial_num,Year),summarize,popvalue=sum(Num_services))
+  simCI <- ddply(simstats,.(Task_ID,Year),summarize,CI20=quantile(popvalue,probs=c(.20)),CI80=quantile(popvalue,probs=c(.80)),CI50 = quantile(popvalue,probs=c(.50)),CI95 = quantile(popvalue,probs=c(.95)),CI05 = quantile(popvalue,probs=c(.05)))
+  simCImelt <- melt(simCI,id.vars=c("Task_ID","Year"),variable.name = "StatType", value.name="PopEstimate")
+  
+  #Calculate 0-4 range, adding up births + 1-4. This will slightly overestimate because births are not a mid-year estimate of kids age 0 alive.
+  years1t4 <- subset(simCImelt,Task_ID=="PopCheck_1t4")
+  births <- subset(simCImelt,Task_ID=="PopCheck_births")
+  years1t4$Task_ID <- "PopCheck_0t4"
+  births$Task_ID <- "PopCheck_0t4"
+  temp0t4 <- left_join(years1t4,births,by=c("Year","StatType","Task_ID"))
+  temp0t4$PopEstimate <- temp0t4$PopEstimate.x+temp0t4$PopEstimate.y
+  temp0t4$PopEstimate.x <- NULL
+  temp0t4$PopEstimate.y <- NULL
+  temp0t4cast <- dcast(temp0t4,Task_ID+Year~StatType)
 
-scenario <- "PopCheck_NationalTotal"
-append <- "_1_1_15_1_125" #fertility p, mortality p, delta fertility p, delta mortality p, delta q
-calib3 <- read.csv(paste("results/results_",scenario,"_",append,".csv",sep=""))
-#Calculate modeled 0-4 pop to match UN estimates.
-#append_temp <- ddply(subset(calib2,Task_ID=="PopCheck_1t4" | Task_ID=="PopCheck_births"),.(Scenario_ID,Run_num,Trial_num,Year,Month),summarize,Num_services=sum(Num_services),Task_ID="PopCheck_0t4",Service_time=0,Health_benefit=NA)
-#calib2 <- rbind(calib2,append_temp)
-#Summarize calibration runs
-simstats3 <- ddply(calib3,.(Task_ID,Run_num,Trial_num,Year),summarize,popvalue=sum(Num_services))
-simCI3 <- ddply(simstats3,.(Task_ID,Year),summarize,CI20=quantile(popvalue,probs=c(.20)),CI80=quantile(popvalue,probs=c(.80)),CI50 = quantile(popvalue,probs=c(.50)),CI95 = quantile(popvalue,probs=c(.95)),CI05 = quantile(popvalue,probs=c(.05)))
-simCImelt3 <- melt(simCI3,id.vars=c("Task_ID","Year"),variable.name = "StatType", value.name="PopEstimate")
+  #Attatch to simCI and simCImelt
+  simCImelt <- rbind(simCImelt,temp0t4)
+  simCI <- rbind(simCI,temp0t4cast)
+    
+  #Calculate differential to median value, simulations
+  simDiff <- simCI
+  simDiff$CI20m50 <- simDiff$CI20 - simDiff$CI50
+  simDiff$CI80m50 <- simDiff$CI80 - simDiff$CI50
+  simDiff$CI05m50 <- simDiff$CI05 - simDiff$CI50
+  simDiff$CI95m50 <- simDiff$CI95 - simDiff$CI50
+  
+  simDiffmelt <- melt(simDiff,id.vars=c("Task_ID","Year"),variable.name = "StatType", value.name="PopDiff2median")
+  simDiffmelt <- subset(simDiffmelt,StatType!= "CI20" & StatType!= "CI80" & StatType!= "CI50" & StatType!= "CI05" & StatType!= "CI95")
+  simDiffmelt$StatType <- factor(simDiffmelt$StatType,labels=unique(simDiffmelt$StatType))
+  
+  #Calculate differential to median value, UN pop
+  UNpop_cast <- dcast(UNpop,Type + Year ~ Stat)
+  UNpop_cast$CI20m50 <- UNpop_cast$Lower20th - UNpop_cast$Median
+  UNpop_cast$CI80m50 <- UNpop_cast$Upper80th - UNpop_cast$Median
+  UNpop_cast$CI05m50 <- UNpop_cast$Lower5th - UNpop_cast$Median
+  UNpop_cast$CI95m50 <- UNpop_cast$Upper95th - UNpop_cast$Median
+  
+  UNdiffmelt <- melt(UNpop_cast,id.vars=c("Type","Year"),variable.name="Stat",value.name="PopDiff2median")
+  UNdiffmelt <- subset(UNdiffmelt,Stat!="Lower20th" & Stat!="Lower5th" & Stat!="Upper95th" & Stat!="Upper80th" & Stat!="Median")
+  
+  
+  #########################
+  ### Make Plots
+  
+  pdf(paste("results/popcalibration_",scenarioname,"_",date,"_",appendtext,".pdf",sep=""),width=8,height=5)
+  
+  ### RELATIVE TO MEDIAN
+  
+  #Annual births
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simDiffmelt,Task_ID=="PopCheck_births" & Year < 2036 & Year > 2020),aes(x=Year,y=PopDiff2median,color=StatType)) + 
+    geom_point(data=subset(UNdiffmelt,Type=="Births_peryr" & Year < 2036),aes(x=Year,y=PopDiff2median*1000,shape=Stat))+labs(title="Annual births",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Children 0-4
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simDiffmelt,Task_ID=="PopCheck_0t4" & Year < 2036 & Year > 2020),aes(x=Year,y=PopDiff2median,color=StatType)) + 
+    geom_point(data=subset(UNdiffmelt,Type=="Children_0t4" & Year < 2036),aes(x=Year,y=PopDiff2median*1000,shape=Stat))+labs(title="Children 0-4",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Children 5-9
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simDiffmelt,Task_ID=="PopCheck_5t9" & Year < 2036 & Year > 2020),aes(x=Year,y=PopDiff2median,color=StatType)) + 
+    geom_point(data=subset(UNdiffmelt,Type=="Children_5t9" & Year < 2036),aes(x=Year,y=PopDiff2median*1000,shape=Stat))+labs(title="Children 5-9",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Children 10-14
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simDiffmelt,Task_ID=="PopCheck_10t14" & Year < 2036 & Year > 2020),aes(x=Year,y=PopDiff2median,color=StatType)) + 
+    geom_point(data=subset(UNdiffmelt,Type=="Children_10t14" & Year < 2036),aes(x=Year,y=PopDiff2median*1000,shape=Stat))+labs(title="Children 10-14",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Population 15-19
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simDiffmelt,Task_ID=="PopCheck_15t19" & Year < 2036 & Year > 2020),aes(x=Year,y=PopDiff2median,color=StatType)) + 
+    geom_point(data=subset(UNdiffmelt,Type=="Adults_15t19" & Year < 2036),aes(x=Year,y=PopDiff2median*1000,shape=Stat))+labs(title="Population 15-19",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Women 15 to 49 Population
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simDiffmelt,Task_ID=="PopCheck_wom15t49" & Year < 2036 & Year > 2020),aes(x=Year,y=PopDiff2median,color=StatType)) + 
+    geom_point(data=subset(UNdiffmelt,Type=="Women_15t49" & Year < 2036),aes(x=Year,y=PopDiff2median*1000,shape=Stat))+labs(title="Women 15 to 49",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Total Population
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simDiffmelt,Task_ID=="PopCheck_total" & Year < 2036 & Year > 2020),aes(x=Year,y=PopDiff2median,color=StatType)) + 
+    geom_point(data=subset(UNdiffmelt,Type=="Total" & Year < 2036),aes(x=Year,y=PopDiff2median*1000,shape=Stat))+labs(title="Total Population",subtitle="Lines=Simulated, Dots=UN Estimate"))
 
-# scenario <- "PopCheck_NationalTotal"
-# append <- "_2_1_15_1_2_2011" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertility calc year
-# calib4 <- read.csv(paste("results/results_",scenario,"_",append,".csv",sep=""))
-# simstats4 <- ddply(calib4,.(Task_ID,Run_num,Trial_num,Year),summarize,popvalue=sum(Num_services))
-# simCI4 <- ddply(simstats4,.(Task_ID,Year),summarize,CI20=quantile(popvalue,probs=c(.20)),CI80=quantile(popvalue,probs=c(.80)),CI50 = quantile(popvalue,probs=c(.50)),CI95 = quantile(popvalue,probs=c(.95)),CI05 = quantile(popvalue,probs=c(.05)))
-# simCImelt4 <- melt(simCI4,id.vars=c("Task_ID","Year"),variable.name = "StatType", value.name="PopEstimate")
+  ### ABSOLUTE VALUES
+  
+  #Annual births
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simCImelt,Task_ID=="PopCheck_births" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
+    geom_point(data=subset(UNpop,Type=="Births_peryr" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Annual births",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Children 0-4
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simCImelt,Task_ID=="PopCheck_0t4" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
+    geom_point(data=subset(UNpop,Type=="Children_0t4" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Children 0-4",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Children 5-9
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simCImelt,Task_ID=="PopCheck_5t9" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
+    geom_point(data=subset(UNpop,Type=="Children_5t9" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Children 5-9",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Children 10-14
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simCImelt,Task_ID=="PopCheck_10t14" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
+    geom_point(data=subset(UNpop,Type=="Children_10t14" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Children 10-14",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Population 15-19
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simCImelt,Task_ID=="PopCheck_15t19" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
+    geom_point(data=subset(UNpop,Type=="Adults_15t19" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Population 15-19",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Women 15 to 49 Population
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simCImelt,Task_ID=="PopCheck_wom15t49" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
+    geom_point(data=subset(UNpop,Type=="Women_15t49" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Women 15 to 49",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  #Total Population
+  print(ggplot()+theme_bw()+
+    geom_line(data=subset(simCImelt,Task_ID=="PopCheck_total" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
+    geom_point(data=subset(UNpop,Type=="Total" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Total Population",subtitle="Lines=Simulated, Dots=UN Estimate"))
+  
+  dev.off()
+}
 
-scenario <- "PopCheck_NationalTotal"
-append <- "_2_1_15_1_2_2005" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertility calc year
-calib5 <- read.csv(paste("results/results_",scenario,"_",append,".csv",sep=""))
-simstats5 <- ddply(calib5,.(Task_ID,Run_num,Trial_num,Year),summarize,popvalue=sum(Num_services))
-simCI5 <- ddply(simstats5,.(Task_ID,Year),summarize,CI20=quantile(popvalue,probs=c(.20)),CI80=quantile(popvalue,probs=c(.80)),CI50 = quantile(popvalue,probs=c(.50)),CI95 = quantile(popvalue,probs=c(.95)),CI05 = quantile(popvalue,probs=c(.05)))
-simCImelt5 <- melt(simCI5,id.vars=c("Task_ID","Year"),variable.name = "StatType", value.name="PopEstimate")
 
-# scenario <- "PopCheck_NationalTotal"
-# append <- "_2_1_15_1_2_2000" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertility calc year
-# calib6 <- read.csv(paste("results/results_",scenario,"_",append,".csv",sep=""))
-# simstats6 <- ddply(calib6,.(Task_ID,Run_num,Trial_num,Year),summarize,popvalue=sum(Num_services))
-# simCI6 <- ddply(simstats6,.(Task_ID,Year),summarize,CI20=quantile(popvalue,probs=c(.20)),CI80=quantile(popvalue,probs=c(.80)),CI50 = quantile(popvalue,probs=c(.50)),CI95 = quantile(popvalue,probs=c(.95)),CI05 = quantile(popvalue,probs=c(.05)))
-# simCImelt6 <- melt(simCI6,id.vars=c("Task_ID","Year"),variable.name = "StatType", value.name="PopEstimate")
+append <- "_15_1_15_1_125_2005" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertilty year
+make_plots(scenario,append)
 
-scenario <- "PopCheck_NationalTotal"
-append <- "_15_1_15_1_125_2005" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertility calc year
-calib7 <- read.csv(paste("results/results_",scenario,"_",append,".csv",sep=""))
-simstats7 <- ddply(calib7,.(Task_ID,Run_num,Trial_num,Year),summarize,popvalue=sum(Num_services))
-simCI7 <- ddply(simstats7,.(Task_ID,Year),summarize,CI20=quantile(popvalue,probs=c(.20)),CI80=quantile(popvalue,probs=c(.80)),CI50 = quantile(popvalue,probs=c(.50)),CI95 = quantile(popvalue,probs=c(.95)),CI05 = quantile(popvalue,probs=c(.05)))
-simCImelt7 <- melt(simCI7,id.vars=c("Task_ID","Year"),variable.name = "StatType", value.name="PopEstimate")
+append <- "_12_12_15_15_125_2005" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertilty year
+make_plots(scenario,append)
 
-#Select a calibration run to plot
-simCImelt <- simCImelt7
+append <- "_12_12_12_20_125_15_2005"
+make_plots(scenario,append)
 
-#Total Population
-ggplot()+theme_bw()+
-  geom_line(data=subset(simCImelt,Task_ID=="PopCheck_total" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
-  geom_point(data=subset(UNpop,Type=="Total" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Total Population",subtitle="Lines=Simulated, Dots=UN Estimate")
+append <- "_12_12_15_20_15_15_2005"
+make_plots(scenario,append)
 
-#Women 15 to 49 Population
-ggplot()+theme_bw()+
-  geom_line(data=subset(simCImelt,Task_ID=="PopCheck_wom15t49" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
-  geom_point(data=subset(UNpop,Type=="Women_15t49" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Women 15 to 49",subtitle="Lines=Simulated, Dots=UN Estimate")
+append <- "_12_12_135_20_135_15_2005"
+make_plots(scenario,append)
 
-#Annual births
-ggplot()+theme_bw()+
-  geom_line(data=subset(simCImelt,Task_ID=="PopCheck_births" & Year < 2036 & Year > 2020),aes(x=Year,y=PopEstimate,color=StatType)) + 
-  geom_point(data=subset(UNpop,Type=="Births_peryr" & Year < 2036),aes(x=Year,y=PopEstimate*1000,shape=Stat))+labs(title="Annual births",subtitle="Lines=Simulated, Dots=UN Estimate")
+append <- "_12_12_135_20_135_15_2005 MortEstimated2020"
+make_plots(scenario,append)
+
+append <- "_12_12_135_20_135_15_2005 MortReported2019"
+make_plots(scenario,append)
+
+append <- "_12_12_135_22_135_16_2005 MortReported2019"
+make_plots(scenario,append)
+
+
+
 
 
--- Run simulations.R
+++ Run simulations.R
@@ -8,104 +8,69 @@ ehep::InitializeScenarios()
 ehep::InitializeStochasticParameters()
 ehep::InitializeSeasonality()
 
-#ehep::SetGlobalStartEndYears(2020, 2035)
-numtrials <- 500
-
-#gpe <- ehep:::globalPackageEnvironment
+numtrials <- 300
+date <- "Feb7"
 
 scenarios <- read_xlsx("config/R Model Inputs.xlsx",sheet="Scenarios")
 
-# Run through the full scenario list. Only use this once scenario manager is working.
-for (i in 1:nrow(scenarios)){
-  print(paste("Startin scenario",i)) 
-  
-  scenario <- scenarios$UniqueID[i]
-  date <- "Jan26"
-  results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-  ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,".csv",sep=""), scenario, 1)
-   
-}
-
-# Baseline National - original guess on p/q values
-scenario <- "PopCheck_NationalTotal"
-append <- "_2_1_15_1_2" #fertility p, mortality p, delta fertility p, delta mortality p, delta q
-results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",append,".csv",sep=""), scenario, 1)
-
-# # National - reduced delta p/q values
-# scenario <- "PopCheck_NationalTotal"
-# append <- "_2_1_15_1_125" #fertility p, mortality p, delta fertility p, delta mortality p, delta q
-# results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-# ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",append,".csv",sep=""), scenario, 1)
-# 
-# # National - reduced fertility p values
-# scenario <- "PopCheck_NationalTotal"
-# append <- "_1_1_15_1_125" #fertility p, mortality p, delta fertility p, delta mortality p, delta q
-# results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-# ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",append,".csv",sep=""), scenario, 1)
-# 
-# # Baseline National - original guess on p/q values + fertility deltas 2019/11
-# scenario <- "PopCheck_NationalTotal"
-# append <- "_2_1_15_1_2_2011" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertility calc year
-# results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-# ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",append,".csv",sep=""), scenario, 1)
-
-# Baseline National - original guess on p/q values + fertility deltas 2019/05
-### Choosing the 2005-->2019 delta fertility rates because they best match birth numbers to UN Population projections through 2035. Did a comparison across all of the possible DHS deltas to 2019 and 2005 had the best match. 
-###This is reasonable because it represents recent time but has enough years to be less sensitive to short-term fluctuations.
-scenario <- "PopCheck_NationalTotal"
-append <- "_2_1_15_1_2_2005" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertility calc year
-results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",append,".csv",sep=""), scenario, 1)
-
-# # Baseline National - original guess on p/q values + fertility deltas 2019/2000
-# scenario <- "PopCheck_NationalTotal"
-# append <- "_2_1_15_1_2_2000" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertility calc year
-# results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-# ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",append,".csv",sep=""), scenario, 1)
+# Run through the full scenario list.
+# for (i in 1:nrow(scenarios)){
+#   print(paste("Startin scenario",i)) 
+#   
+#   scenario <- scenarios$UniqueID[i]
+#   results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
+#   ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,".csv",sep=""), scenario, 1)
+#    
+# }
 
 ehep::InitializeStochasticParameters()
-scenario <- "PopCheck_NationalTotal"
-append <- "_15_1_15_1_125_2005" #fertility p, mortality p, delta fertility p, delta mortality p, delta q, baseline fertility calc year
+scenario <- "PopTest_NtlTotal"
+append <- "_15_1_15_1_125_2005" #fertility p, mortality p, delta fertility p, delta mortality p, fert/mort delta q, baseline fertility calc year
 results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",append,".csv",sep=""), scenario, 1)
+ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,"_",append,".csv",sep=""), scenario, 1)
 
 
-
-########################################### 
-# First Draft GR deck
-
-# Baseline Rural expected value
-scenario <- "ExpectedValue_RuralRatio"
+ehep::InitializeStochasticParameters()
+scenario <- "PopTest_NtlTotal"
+append <- "_12_12_15_15_125_2005" #fertility p, mortality p, delta fertility p, delta mortality p, fert/mort delta q, baseline fertility calc year
 results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-ehep::SaveSuiteResults(results, paste("results/results_",scenario,".csv",sep=""), scenario, 1)
+ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,"_",append,".csv",sep=""), scenario, 1)
 
-# No fertility decrease, rural expected value
-# o_Fertility should be false, but set it to true.
-# Set delta rate to 1.0 in PopValues fertility rows 5,17-22.
-scenario <- "NoFertilityChange_RuralRatio"
+
+ehep::InitializeStochasticParameters()
+scenario <- "PopTest_NtlTotal"
+append <- "_12_12_12_20_125_15_2005" #fertility p, mortality p, delta fertility p, delta mortality p, fertility delta q, mortality delta q, baseline fertility calc year
 results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-ehep::SaveSuiteResults(results, paste("results/results_",scenario,".csv",sep=""), scenario, 1)
+ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,"_",append,".csv",sep=""), scenario, 1)
 
-# No gains in HIV, malaria, or TB. 
-# o_MHIVTB_decr should be false, doesn't matter currently bc/ doesn't function.
-# Set Annual Deltas in TaskValues sheet for M, TB, HIV to 1.0.
-scenario <- "p_ChildHealthImp_RR"
+
+ehep::InitializeStochasticParameters()
+scenario <- "PopTest_NtlTotal"
+append <- "_12_12_15_20_15_15_2005" #fertility p, mortality p, delta fertility p, delta mortality p, fertility delta q, mortality delta q, baseline fertility calc year
 results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-ehep::SaveSuiteResults(results, paste("results/results_",scenario,".csv",sep=""), scenario, 1)
+ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,"_",append,".csv",sep=""), scenario, 1)
 
-# No gains in child health. 
-# o_ChildDis_decr should be false, doesn't matter currently bc/ doesn't function.
-# Set Annual Deltas in TaskValues sheet for sick child care rows to 1.0.
-scenario <- "p_Demographics_RR"
+
+ehep::InitializeStochasticParameters()
+scenario <- "PopTest_NtlTotal"
+append <- "_12_12_135_20_135_15_2005" #fertility p, mortality p, delta fertility p, delta mortality p, fertility delta q, mortality delta q, baseline fertility calc year
 results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-ehep::SaveSuiteResults(results, paste("results/results_",scenario,".csv",sep=""), scenario, 1)
+ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,"_",append,".csv",sep=""), scenario, 1)
 
-# No seasonality.
-# Set o_Seasonality to FALSE in row 4. This is not strictly the right way to do this (there should be an intermediate step of population growth, but that doesn't work yet.)
-# Will need to rerun this later.
-scenario <- "p_Seasonality_RR"
+ehep::InitializeStochasticParameters()
+scenario <- "PopTest_NtlTotal"
+append <- "_12_12_135_20_135_15_2005 MortEstimated2020" #fertility p, mortality p, delta fertility p, delta mortality p, fertility delta q, mortality delta q, baseline fertility calc year
 results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-ehep::SaveSuiteResults(results, paste("results/results_",scenario,".csv",sep=""), scenario, 1)
+ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,"_",append,".csv",sep=""), scenario, 1)
 
+ehep::InitializeStochasticParameters()
+scenario <- "PopTest_NtlTotal"
+append <- "_12_12_135_20_135_15_2005 MortReported2019" #fertility p, mortality p, delta fertility p, delta mortality p, fertility delta q, mortality delta q, baseline fertility calc year
+results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
+ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,"_",append,".csv",sep=""), scenario, 1)
 
+ehep::InitializeStochasticParameters()
+scenario <- "PopTest_NtlTotal"
+append <- "_12_12_135_22_135_16_2005 MortReported2019" #fertility p, mortality p, delta fertility p, delta mortality p, fertility delta q, mortality delta q, baseline fertility calc year
+results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
+ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,"_",append,".csv",sep=""), scenario, 1)
