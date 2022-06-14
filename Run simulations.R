549c8f5b237b970d78b3ca4e70757613f63a313b
Brittany Hagedorn <bhagedorn@idmod.org>
2022-06-14 16:50:52 -0700

    Added scenario naming convention

--- Run simulations.R
+++ Run simulations.R
@@ -10,8 +10,8 @@ ehep::InitializeScenarios()
 ehep::InitializeStochasticParameters()
 ehep::InitializeSeasonality()
 
-numtrials <- 500
-date <- "March23_ECOruns"
+numtrials <- 100
+date <- "May2_2022"
 
 scenarios <- read_xlsx("config/R Model Inputs.xlsx",sheet="Scenarios")
 
@@ -20,8 +20,9 @@ for (i in 1:nrow(scenarios)){
   print(paste("Starting scenario",i))
 
   scenario <- scenarios$UniqueID[i]
+  geoname <- scenarios$Geography_dontedit[i]
   results <-   ehep::RunExperiments(scenarioName = scenario, trials = numtrials, debug = FALSE)
-  ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",date,".csv",sep=""), scenario, 1)
+  ehep::SaveSuiteResults(results, paste("results/results_",scenario,"_",geoname,"_",date,".csv",sep=""), scenario, 1)
 
 }
 
