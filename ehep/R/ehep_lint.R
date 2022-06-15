8ec286978973ca371e208db3477e7a0bfd308e39
Charles Eliot <charles.eliot@gatesfoundation.org>
2022-06-15 14:41:27 -0700

    Added lint support for seasonality task checking

--- ehep/R/ehep_lint.R
+++ ehep/R/ehep_lint.R
@@ -1,10 +1,12 @@
+# Completion codes
+
 .Success <- 0L
 .errInputFileNotFound <- 1L
 .errInputFileNotReadable <- 2L
 .errScenarioSheetNotFound <- 4L
 .errScenarioSheetNotReadable <- 8L
-
-
+.errSeasonalityOffsetSheetNotFound <- 16L
+.errSeasonalityOffsetSheetNotReadable <- 32L
 
 #' Perform Sanity Checks On Input Excel File
 #'
@@ -22,7 +24,8 @@
 #' @export
 CheckInputExcelFileFormat <- function(inputFile = NULL,
                                       outputFile = NULL,
-                                      scenarioSheet = "Scenarios") {
+                                      scenarioSheet = "Scenarios",
+                                      seasonalityOffsetsSheet = "SeasonalityOffsets") {
   errcode <- .Success
 
   if (is.null(inputFile)) {
@@ -30,26 +33,28 @@ CheckInputExcelFileFormat <- function(inputFile = NULL,
   }
 
   if (is.null(outputFile)){
-    errcode <- .checkInputExcelFileFormat(inputFile, scenarioSheet)
+    errcode <- .checkInputExcelFileFormat(inputFile, scenarioSheet, seasonalityOffsetsSheet)
   } else {
     withr::with_output_sink(outputFile, code = {
-      errcode <- .checkInputExcelFileFormat(inputFile, scenarioSheet)
+      errcode <- .checkInputExcelFileFormat(inputFile, scenarioSheet, seasonalityOffsetsSheet)
     })
   }
 
   return(errcode)
 }
 
-.checkInputExcelFileFormat <- function(inputFile, scenarioSheet){
+.checkInputExcelFileFormat <- function(inputFile, scenarioSheet, seasonalityOffsetsSheet){
   .catLine()
   .catLine(date())
   .catLine("Input file = ", inputFile)
 
+  # Create a place for the criticalChecks() functions to write some return
+  # values back to the calling code.
   e <- new.env(parent = environment())
 
   # The first set of checks are critical: if these checks fail something is
   # so wrong there's no way to continue.
-  errcode <- .criticalChecks(inputFile, scenarioSheet, e)
+  errcode <- .criticalChecks(inputFile, scenarioSheet, seasonalityOffsetsSheet, e)
 
   if (errcode != .Success){
     return(errcode)
@@ -57,11 +62,9 @@ CheckInputExcelFileFormat <- function(inputFile = NULL,
 
   # The rest of the checks expose serious, but not necessarily fatal, problems
 
+  # Recover returned values from the by-ref environment
   scenarios <- e$scenarios
   sheets <- e$sheets
-
-  print(scenarios)
-  print(sheets)
 
   # Collect references to other workbook sheets, and check that all the
   # references are to existing sheets
@@ -80,12 +83,80 @@ CheckInputExcelFileFormat <- function(inputFile = NULL,
     .catLine("* ", sheetRefs[results == FALSE])
   }
 
+  # ------------------------------------------
 
+  seasonalityOffsets <- e$seasonalityOffsets
+
+  # Check whether any tasks are duplicated in the seasonality offsets table
+  tasks <- seasonalityOffsets$Task
+  dups <- duplicated(tasks)
+
+  if (!any(dups)){
+    .catLine("No duplicate tasks in seasonality offsets table ... OK")
+  } else {
+    .catLine("The following tasks are duplicated in the seasonality offsets table ...")
+    .catLine("* ", tasks[dups])
+  }
+
+
+
+
+
+  # Generate a list of unique, valid Task Value sheet names
+  taskValuesSheets <- unique(scenarios$sheet_TaskValues)
+  taskValuesSheets <- taskValuesSheets[taskValuesSheets %in% sheets]
+
+  tasksInSeasonalityOffsetsSheet <- unique(tasks)
+
+
+
+
+
+
+  l <- lapply(taskValuesSheets, function(sheet){
+    tvs <- tryCatch({
+      readxl::read_xlsx(inputFile, sheet = sheet)
+    },
+    warning = function(c){return(NULL)},
+    error = function(c){return(NULL)})
+
+    if (is.null(tvs)){
+      return(character(0))
+    } else {
+      tasks <- tvs$Indicator
+      used <- tasksInSeasonalityOffsetsSheet %in% tasks
+      return(tasksInSeasonalityOffsetsSheet[!used])
+    }
+  })
+
+  .catLine("The following tasks in the scenario offsets table are not used in task values sheets ...")
+
+  for (i in seq_along(taskValuesSheets)){
+    sheet <- taskValuesSheets[i]
+    unusedSeasonalityTasks <- l[[i]]
+
+    if (length(unusedSeasonalityTasks) == 0){
+      .catLine("- ", sheet, " : ", "OK")
+    } else {
+      .catLine("* ", sheet, " : ", unusedSeasonalityTasks)
+    }
+  }
+
+  # tasksInTvSheets <- unlist(l)
+  #
+  # used <- tasksInSeasonalityOffsetsSheet %in% tasksInTvSheets
+  #
+  # if (all(used)){
+  #   .catLine("All the tasks in the scenario offsets table are used in task values sheets ... OK")
+  # } else {
+  #   .catLine("The following tasks in the scenario offsets table are not used in task values sheets ...")
+  #   .catLine("* ", tasksInSeasonalityOffsetsSheet[used == FALSE])
+  # }
 
   return(.Success)
 }
 
-.criticalChecks <- function(inputFile, scenarioSheet, outEnv){
+.criticalChecks <- function(inputFile, scenarioSheet, seasonalityOffsetsSheet, outEnv){
   outEnv$sheets <- NULL
   outEnv$scenarios <- NULL
 
@@ -115,7 +186,13 @@ CheckInputExcelFileFormat <- function(inputFile = NULL,
     return(.errScenarioSheetNotFound)
   }
 
-  # Read the contents of the scenarios sheet
+  # Check whether the seasonality offsets sheet is in the sheet list
+  if (!(seasonalityOffsetsSheet %in% sheets)){
+    .catLine(seasonalityOffsetsSheet, " sheet could not be found")
+    return(.errSeasonalityOffsetSheetNotFound)
+  }
+
+  # Read the contents of the Scenarios sheet
   scenarios <- tryCatch({
     readxl::read_xlsx(inputFile, sheet = scenarioSheet)
   },
@@ -123,14 +200,25 @@ CheckInputExcelFileFormat <- function(inputFile = NULL,
   error = function(c){return(NULL)})
 
   if (is.null(scenarios)){
-    if (!(scenarioSheet %in% tabs)){
-      .catLine("Could not read scenario sheet (", scenarioSheet, ")")
-      return(.errScenarioSheetNotReadable)
-    }
+    .catLine("Could not read scenario sheet (", scenarioSheet, ")")
+    return(.errScenarioSheetNotReadable)
+  }
+
+  # Read the contents of the SeasonalityOffsets sheet
+  seasonalityOffsets <- tryCatch({
+    readxl::read_xlsx(inputFile, sheet = seasonalityOffsetsSheet)
+  },
+  warning = function(c){return(NULL)},
+  error = function(c){return(NULL)})
+
+  if (is.null(seasonalityOffsets)){
+    .catLine("Could not read seasonality offsets sheet (", seasonalityOffsetsSheet, ")")
+    return(.errSeasonalityOffsetSheetNotReadable)
   }
 
   outEnv$sheets <- sheets
   outEnv$scenarios <- scenarios
+  outEnv$seasonalityOffsets <- seasonalityOffsets
 
   return(.Success)
 }
--- ehep/tests/testthat/test-ehep_lint.R
+++ ehep/tests/testthat/test-ehep_lint.R
@@ -8,13 +8,24 @@ test_that("Lint: basic", {
   local_vars("inputExcelFile", envir = e)
   e$inputExcelFile <- "./simple_config/Test Inputs.xlsx"
 
-  outFile = "lintout_01.txt"
+  outFile = "lintout_01_clean.txt"
   errCode <- ehep::CheckInputExcelFileFormat(outputFile = outFile)
   testthat::expect_true(file.exists(outFile))
 })
 
-# Bad name for the scenario tab
-test_that("Lint: bad scenario tab name", {
+# Basic operation: use default config file and scenario tab, redirect output to a file
+test_that("Lint: basic", {
+  e <- ehep:::GPE
+  local_vars("inputExcelFile", envir = e)
+  e$inputExcelFile <- "./bad_config/Test Inputs - malformed.xlsx"
+
+  outFile = "lintout_01_dirty.txt"
+  errCode <- ehep::CheckInputExcelFileFormat(outputFile = outFile)
+  testthat::expect_true(file.exists(outFile))
+})
+
+# Bad name for the scenarios sheet
+test_that("Lint: bad scenario steet name", {
   e <- ehep:::GPE
   local_vars("inputExcelFile", envir = e)
   e$inputExcelFile <- "./simple_config/Test Inputs.xlsx"
@@ -23,6 +34,18 @@ test_that("Lint: bad scenario tab name", {
   errCode <- ehep::CheckInputExcelFileFormat(outputFile = outFile, scenarioSheet = "notasheet")
   testthat::expect_true(file.exists(outFile))
   testthat::expect_equal(errCode, ehep:::.errScenarioSheetNotFound)
+})
+
+# Bad name for the seasonality offsets sheet
+test_that("Lint: bad seasonality offsets sheet name", {
+  e <- ehep:::GPE
+  local_vars("inputExcelFile", envir = e)
+  e$inputExcelFile <- "./simple_config/Test Inputs.xlsx"
+
+  outFile = "lintout_05.txt"
+  errCode <- ehep::CheckInputExcelFileFormat(outputFile = outFile, seasonalityOffsetsSheet = "notasheet")
+  testthat::expect_true(file.exists(outFile))
+  testthat::expect_equal(errCode, ehep:::.errSeasonalityOffsetSheetNotFound)
 })
 
 # Bad name for the input file
