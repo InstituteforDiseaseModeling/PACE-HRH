0834c3f0aa1bb412491b672caf83387090ca4f3e
mewu <mewu@idmod.org>
2022-07-21 15:37:50 -0700

    try to make dep install faster

---
+++ ehep/tests/install_dep_for_tests.R
@@ -0,0 +1,31 @@
+library(stringr)
+library(parallel)
+getDepPackages = function(descriptionpath) {
+  fp = file(descriptionpath, "r")
+  start = F
+  done = F
+  while ( !done ) {
+    line = readLines(fp, n = 1)
+    if (grepl("Imports:",line)) {
+      start = T
+      next
+    }
+    else if (start) {
+      if (!grepl(":", line)) {
+        dep_name = gsub(',', '', str_trim(line))
+        print(paste("install", dep_name, "with", detectCores(), "cores", sep=" "))
+        options(install.packages.compile.from.source = "always")
+        cmd <- paste( "install.packages('", dep_name, "',", "Ncpus = ", detectCores(), ")", sep="")
+        eval(parse(text=cmd))
+      }
+      else {
+        start = F
+        done = T
+        break
+      }
+    }
+  }
+  close(fp)
+}
+
+getDepPackages("../DESCRIPTION")
