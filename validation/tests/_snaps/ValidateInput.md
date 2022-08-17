d9a618cfd7461633df7e1b91d202d330f9f84173
mewu <mewu@idmod.org>
2022-08-17 16:45:20 -0700

    #70 adding validation for capturing input data issues

---
+++ validation/tests/_snaps/ValidateInput.md
@@ -0,0 +1,13 @@
+# Validation capture
+
+    Code
+      ValidateInputExcelFileContent(inputFile = "tests/sample_config/Test_validation.xlsx",
+        outputDir = logdir, sheetNames = c("SeasonalityCurves"))
+    Message <simpleMessage>
+      Rule failed but No record-wise info:  Error in validate::violating(target, out[i]): Not all rules have record-wise output
+      
+    Message <rlang_message>
+      Joining, by = "name"
+    Output
+      [1] -11
+
---
+++ validation/tests/test_ValidateInput.R
@@ -0,0 +1,51 @@
+context("Test ValidateInputExcelFileContent")
+local_edition(3)
+setwd("..")
+source("ValidateInput.R")
+
+# test validate no rules
+test_that("Validation no rules", {
+  # test should fail if no rule is defined for the sheet
+  testthat::expect_error(ValidateInputExcelFileContent(inputFile = "tests/sample_config/Test_validation.xlsx", sheetNames = c("Total_Pop2")), "rule not found")
+  
+})
+# test validation capture
+test_that("Validation capture", {
+  errCode <- .errValidationRuleFailed
+  # In this example rules Total_diarrhea does not sum to 1, this is a table-wise error not row-wise error
+  # The error is captured and verified in _snaps/ehep_lint.md
+  
+  logdir <- tempdir()
+  # skip_if(.Platform$OS.type != "windows")
+  testthat::expect_snapshot(ValidateInputExcelFileContent(inputFile = "tests/sample_config/Test_validation.xlsx",
+                                                                outputDir = logdir,
+                                                                sheetNames = c("SeasonalityCurves")))
+  
+})
+# test validation success
+test_that("Validation capture Success", {
+  errCode <- .Success
+  
+  logdir <- tempdir()
+  testthat::expect_equal(errCode, ValidateInputExcelFileContent(inputFile = "tests/sample_config/Test_validation2.xlsx",
+                                                                      outputDir = logdir,
+                                                                      sheetNames = c("SeasonalityCurves")))
+  
+})
+
+# test validation row-wise
+test_that("Validation capture Success", {
+  errCode <- .Success
+  # In this example, rule is set as warning level so violation is not causing error but output files will be available
+  logdir <- tempdir()
+  testthat::expect_equal(errCode, ValidateInputExcelFileContent(inputFile = "tests/sample_config/Test_validation.xlsx",
+                                                                      outputDir = logdir,
+                                                                      sheetNames = c("TaskValues_ref")))
+  result_file <- paste(logdir, "info_violation_Missing_parameter_value.csv", sep="/")
+  testthat::expect_true(file.exists(result_file))
+  violations <- read.csv(result_file)
+  # search for the row that fails Indicator = FH.MN.ANC.1XX
+  violating_row <- violations %>%
+    dplyr::filter(Indicator == "FH.MN.ANC.1XX")
+  testthat::expect_equal(nrow(violating_row ), 1)
+})
