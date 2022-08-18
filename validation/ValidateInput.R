34da00ec478f590199f484b19bac7f6446a56001
MeWu-IDM <40474438+MeWu-IDM@users.noreply.github.com>
2022-08-18 10:12:12 -0700

    validation tool #70
    adding validation for capturing input data issues

---
+++ validation/ValidateInput.R
@@ -0,0 +1,132 @@
+options(install.packages.check.source = "no")
+packages = c("validate","tidyverse", "dplyr","ggplot2")
+for(i in packages){
+  if(!require(i, character.only = T)){
+    install.packages(i)
+    library(i, character.only = T)
+  }
+}
+
+.Success <- 0L
+.errValidationRuleFailed <- -11L
+
+
+#' Perform Validation on Input Excel File
+#'
+#' @description
+#' Validate if all inputs comply with parameter rules
+#'
+#' @param inputFile Excel file to examine. If NULL, check the file defined in the global Configuration.
+#' @param outputDir Results output directory. default is in log folder
+#' @param sheetNames A vector of names of the sheets to be validate
+#'
+#' @return Error code.
+#' 0 = Success
+#' -1 = validation error
+#'
+#' @export
+ValidateInputExcelFileContent <- function(inputFile,
+                                          outputDir = "log",
+                                          sheetNames = NULL){
+  errcode <- .Success
+  
+  # check all available rules
+  if (is.null(sheetNames)){
+    sheetNames <- gsub("^rules_([A-Za-z_0-9]+).yaml$", "\\1", list.files("validation/rules"))
+  }
+  
+  dir.create(outputDir, showWarnings = FALSE)
+  
+  # loop over each sheet and apply corresponding rules
+  result <- data.frame()
+  rules_combined <- data.frame()
+  plots <- vector()
+  for (i in sheetNames){
+    f = file.path("validation/rules", paste("rules_", i, ".yaml", sep=""))
+    if (!file.exists(f)){
+      stop(paste("rule not found for:", i))
+    }
+    else{
+      rules <- validate::validator(.file=f)
+      if (length(rules_combined)==0){
+        rules_combined <- validate::as.data.frame(rules)
+      }
+      else {
+        rules_combined <- rbind(rules_combined, validate::as.data.frame(rules))
+      }
+      
+      data <- readxl::read_xlsx(inputFile, sheet = i)
+      data_target <- data.frame(data)
+      
+      # Confront the rules and check if any error occurs
+      out <- validate::confront(data_target, rules,  lin.ineq.eps=0, lin.eq.eps=1e-08)
+      if(TRUE %in% validate::summary(out)$error){
+        stop(paste("Some error occurred evaluating rules:", i))
+      }
+      # Apply rules and save the violation results
+      check <- .get_violation_rows(out, data_target, rules, outputDir)
+      errcode = min(errcode, check)
+      
+      # combine results in the loop
+      if (length(result)==0){
+        result <- validate::as.data.frame(validate::summary(out))
+      }
+      else {
+        result <- rbind(result,  validate::as.data.frame(validate::summary(out)))
+      }
+      
+    }
+  }
+  
+  # summary and plot check result
+  final_result <- rules_combined %>%
+    dplyr::select(c("name", "severity")) %>%
+    dplyr::inner_join(result) %>%
+    dplyr::select(c("name", "severity", "items", "fails", "passes", "expression")) %>%
+    tidyr::pivot_longer(cols=c("passes", "fails"), names_to = "result", values_to = "total") %>%
+    dplyr::mutate(result = dplyr::if_else(severity != 'error' & result == 'fails', severity, result))
+  
+  p <- ggplot2::ggplot(final_result, aes(x=total/items, y=name, fill=result)) +
+    ggplot2::geom_bar(stat = "identity") +
+    ggplot2::scale_fill_manual(values = c("passes"="green", "fails"="red", "warning"="yellow", "info"="blue")) +
+    ggplot2::scale_x_continuous(labels=scales::percent) +
+    ggplot2::geom_text(data=subset(final_result,total> 0),ggplot2::aes(label=total, y =name), size=2, position=position_fill()) +
+    ggplot2::ggtitle("Input Spreadsheet Validation Results")
+  ggplot2::ggsave(file.path(outputDir, "input_validation_results.png"), p, width = 160, height = 100, units="mm")
+  
+  return (errcode)
+}
+
+
+.get_violation_rows <- function(out, target, rules, outputDir){
+  errcode <- .Success
+  for (i in names(out)){
+    severity <- rules[[i]]@meta$severity
+    df_violations <- NULL
+    if (nrow(validate::values(out[i])) > 1 | validate::values(out[i])[[1]] == FALSE){
+      if (severity == "error"){
+        errcode <- .errValidationRuleFailed
+      }
+      tryCatch(
+        {
+          df_violations <- validate::violating(target, out[i])
+        },
+        error=function(e){
+          message(paste("Rule failed but No record-wise info: ", e))
+          df_violations <- NULL
+          return (df_violations)
+        }
+      )
+      if (!is.null(df_violations) & length(df_violations) > 0){
+        # only fail the validation for critical rules
+        # severity <- validate::as.data.frame(rules) %>% dplyr::filter(name==i) %>% dplyr::select(severity)
+       
+        if (severity == "error"){
+          errcode <- .errValidationRuleFailed
+        }
+        write.csv(df_violations, file.path(outputDir, paste(severity, "_violation_", i, ".csv", sep="")))
+      }
+    }
+  }
+  return (errcode)
+}
