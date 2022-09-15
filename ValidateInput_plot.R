5d653808eaf62aedefb6fbde8071985c6ba55186
mewu <mewu@idmod.org>
2022-09-15 14:59:54 -0700

    fix ordering

--- ValidateInput_plot.R
+++ ValidateInput_plot.R
@@ -12,10 +12,12 @@ plot_result <- function(result_file, outfile, fancy = TRUE){
     select(c("name","items","passes","warning","info","fails")) %>%
     pivot_longer(cols=c("passes","warning","info","fails"), names_to="result", values_to="total")
   
+  ylevel_order <- rev(final_result %>% select(name) %>% unique() %>% arrange(name) %>% .$name)
+  xlevel_order <- c("info", "fails", "warning", "passes")
   
   # a normal plot...
   if (!fancy){
-    p <- ggplot(final_result, aes(x=total/items, y=name, fill=result)) +
+    p <- ggplot(final_result, aes(x=total/items, y=factor(name, level=ylevel_order), fill=factor(result, level=xlevel_order))) +
       geom_bar(stat = "identity") +
       xlab(NULL) + ylab(NULL) +
       scale_fill_manual(values = c("passes"="limegreen", "fails"="orangered", "warning"="darkgoldenrod1", "info"="dodgerblue")) +
@@ -33,7 +35,6 @@ plot_result <- function(result_file, outfile, fancy = TRUE){
         legend.title=element_blank(),
         panel.background=element_rect(fill = "white"),
         text=element_text(size=11))
-    
     ggsave(outfile, p, width = 160, height = 160, units="mm")
   }
   else{
@@ -41,7 +42,7 @@ plot_result <- function(result_file, outfile, fancy = TRUE){
     font_add_google(name="Barlow Condensed", family="barlow-cond")
     showtext_auto()
     
-    p <- ggplot(final_result, aes(x=total/items, y=name, fill=result)) +
+    p <- ggplot(final_result, aes(x=total/items, y=factor(name, level=ylevel_order), fill=factor(result, level=xlevel_order))) +
       geom_bar(stat = "identity") +
       xlab(NULL) + ylab(NULL) +
       scale_fill_manual(values = c("passes"="limegreen", "fails"="orangered", "warning"="darkgoldenrod1", "info"="dodgerblue")) +
