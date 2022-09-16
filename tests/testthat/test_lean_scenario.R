ab450708432624052232ff7a749572fef48f4d14
mewu <mewu@idmod.org>
2022-09-16 16:24:49 -0700

    Update tests and build script

--- tests/testthat/test_lean_scenario.R
+++ tests/testthat/test_lean_scenario.R
@@ -8,14 +8,14 @@ for(i in packages){
   }
 }
 
-library(ehep)
+library(pacehrh)
 setwd("../..")
-minimum_input_file <- "ehep/inst/extdata/R Model Inputs_template_lean.xlsx"
+minimum_input_file <- "pacehrh/inst/extdata/R Model Inputs_template_lean.xlsx"
 
 # Set up necessary steps for default minumum template
 test_template <- function(){
     dir.create("tests/results", showWarnings = FALSE)
-    ehep:::setGlobalConfig(inputExcelFilePath = minimum_input_file)
+    pacehrh:::setGlobalConfig(inputExcelFilePath = minimum_input_file)
     Trace(TRUE)
     InitializePopulation()
     InitializeHealthcareTasks()
@@ -31,7 +31,7 @@ test_that("check_template", {
   # There is currently warning about 
   # tasks in the scenario offsets table are not used in task values sheets
   # We need to define the validate behavior in lint
-  expect_true(ehep::CheckInputExcelFileFormat(minimum_input_file) %in% c(ehep:::.Success, ehep:::.warnProblemsFound))
+  expect_true(pacehrh::CheckInputExcelFileFormat(minimum_input_file) %in% c(pacehrh:::.Success, pacehrh:::.warnProblemsFound))
   
 })
 
@@ -41,10 +41,10 @@ test_that("simple regression", {
     numtrials <- 2
     dir.create("tests/results/regression", showWarnings = FALSE)
     # Run through the full scenario list.
-    for (i in 1:nrow(ehep:::GPE$scenarios)){
+    for (i in 1:nrow(pacehrh:::GPE$scenarios)){
       cat(paste("Starting scenario",i))
-      scenario <- ehep:::GPE$scenarios$UniqueID[i]
-      geoname <- ehep:::GPE$scenarios$Geography_dontedit[i]
+      scenario <- pacehrh:::GPE$scenarios$UniqueID[i]
+      geoname <- pacehrh:::GPE$scenarios$Geography_dontedit[i]
       results <- RunExperiments(scenarioName = scenario, trials = numtrials, debug = TRUE)
       expect_true(all(!is.na(results)))
       filename <- paste("tests/results/regression/results_", geoname, scenario,".csv",sep="")
