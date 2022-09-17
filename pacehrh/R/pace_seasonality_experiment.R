517d36d4eb00e8f281f4c310d999fc53bc730903
Charles Eliot <Charles.Eliot@gatesfoundation.org>
2022-09-17 12:28:04 -0700

    Simplify results structure: replace type-specific list sections with big matrices.

--- pacehrh/R/pace_seasonality_experiment.R
+++ pacehrh/R/pace_seasonality_experiment.R
@@ -18,20 +18,15 @@ runSeasonalityExperiment <- function(results, debug = FALSE){
 
   # Look for seasonality-affected tasks in the task list for this scenario
 
-  # Quick hack to remove PopType filter. TODO: better version!
-  taskIds <- seq_along(BVE$taskData$Geography)
-
-  taskNames <- BVE$taskData$Indicator[taskIds]
+  taskNames <- BVE$taskData$Indicator
   seasonalityTaskNames <- GPE$seasonalityOffsets$Task
   seasonalityTaskCurves <- GPE$seasonalityOffsets$Curve
 
-  l <- lapply(seq_along(taskIds), function(i){
-    taskName <- taskNames[i]
-
+  l <- lapply(taskNames, function(taskName){
     if (length(seasonalityTaskIndex <- which(seasonalityTaskNames == taskName)) == 1){
       # Grab already computed annual data for this task
-      annualTimes <- results$Clinical$Time[, taskName]
-      annualServices <- results$Clinical$N[, taskName]
+      annualTimes <- results$AnnualTimes[taskName,]
+      annualServices <- results$AnnualCounts[taskName,]
 
       # Find the appropriate seasonality curve
       curve <-
@@ -49,7 +44,7 @@ runSeasonalityExperiment <- function(results, debug = FALSE){
 
       # Build a scatter matrix
       nRows <- nCols <- length(monthlyServices)
-      assertthat::are_equal(nRows, 12 * length(GPE$years))
+#      assertthat::are_equal(nRows, 12 * length(GPE$years))
 
       A <- matrix(0, nrow = nRows, ncol = nCols)
 
@@ -74,20 +69,8 @@ runSeasonalityExperiment <- function(results, debug = FALSE){
 
       return(list(Time = monthlyTimes, N = monthlyServices))
     } else {
-      annualServices <- NULL
-      annualTimes <- NULL
-
-      # Find the previous calculated values
-      for (type in GPE$taskTypes){
-        if (!is.null(results[[type]])){
-          tnames <- dimnames(results[[type]]$Time)[[2]]
-          if (taskName %in% tnames){
-            annualTimes <- results[[type]]$Time[, taskName]
-            annualServices <- results[[type]]$N[, taskName]
-            break
-          }
-        }
-      }
+      annualTimes <- results$AnnualTimes[taskName,]
+      annualServices <- results$AnnualCounts[taskName,]
 
       names(annualTimes) <- NULL
       monthlyTimes <- .convertAnnualToMonthly(annualTimes, dummyCurve)
