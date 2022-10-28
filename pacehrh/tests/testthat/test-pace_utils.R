90e21c467e9212d0cc3a6e8b3d38e5271f6f8be3
Charles Eliot <Charles.Eliot@gatesfoundation.org>
2022-10-28 15:18:13 -0700

    Add GetSuiteRates() to return entire suite of fertility/mortality rates

--- pacehrh/tests/testthat/test-pace_utils.R
+++ pacehrh/tests/testthat/test-pace_utils.R
@@ -10,3 +10,37 @@ test_that("Utilities: is.blank", {
   testthat::expect_false(pacehrh:::is.blank(0))
   testthat::expect_false(pacehrh:::is.blank(TRUE))
 })
+
+test_that("Utilities: GetSuiteRates", {
+  pacehrh::InitializePopulation()
+  pacehrh::InitializeScenarios()
+  pacehrh::InitializeStochasticParameters()
+  pacehrh::InitializeSeasonality()
+
+  scenario <- "MergedModel"
+  nTrials <- 5
+  startYear <- 2025
+  endYear <- 2050
+  nMonths <- 12 * length(startYear:endYear)
+
+  shoulderYears <- pacehrh:::GPE$shoulderYears
+
+  pacehrh::SetGlobalStartEndYears(startYear, endYear)
+
+  results <-
+    pacehrh::RunExperiments(scenarioName = scenario,
+                            trials = nTrials)
+
+
+  testthat::expect_warning(pacehrh::GetSuiteRates(results, rateCategory = "notacategory"))
+
+  df <- pacehrh::GetSuiteRates(results, rateCategory = "femaleFertility")
+  testthat::expect_true(!is.null(df))
+
+  cols <- c("Label",
+            "Trial",
+            "Year",
+            "Rate")
+
+  testthat::expect_equal(names(df), cols)
+})
