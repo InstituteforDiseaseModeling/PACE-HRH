7384fb01eded8137df4a1329a9e8310d2ba049b2
Charles Eliot <Charles.Eliot@gatesfoundation.org>
2023-07-27 08:34:01 -0700

    More lint-related formatting fixes.

--- pacehrh/R/pace_task_time_stochasticity.R
+++ pacehrh/R/pace_task_time_stochasticity.R
@@ -5,22 +5,22 @@
 #' @return Adjusted task parameters matrix
 #'
 #' @noRd
-varyTaskValues <- function(tasks){
+varyTaskValues <- function(tasks) {
   # Do nothing if stochasticity is turned off
-  if (!GPE$stochasticity){
+  if (!GPE$stochasticity) {
     return(tasks)
   }
 
   pars <- BVE$stochasticParams
 
   # Tweak MinsPerContact value
-  m <- tasks[,"MinsPerContact"]
+  m <- tasks[, "MinsPerContact"]
   nonZeroMask <- ((!is.na(m)) & (m > 0))
 
-  p = pars[pars$Value == "Minutes per contact",]$p
+  p <- pars[pars$Value == "Minutes per contact", ]$p
 
   # Generate log-normal distribution with mean = 1 and sd = p
-  logm <- - (log(1 + p^2))/2
+  logm <- -(log(1 + p^2)) / 2
   logsd <- sqrt(log((1 + p^2)))
   samples <- rlnorm(sum(nonZeroMask), logm, logsd)
 
