a8ed02dd188d181c537cf85e120b8bf220b21e63
Emily Claps <108016846+emilykathryn@users.noreply.github.com>
2024-07-03 13:53:40 -0700

    added function to verify a column contains only numeric values

--- util/data_validation.R
+++ util/data_validation.R
@@ -1,17 +1,27 @@
-#' #' Perform validation on a data column
+#' Perform validation on a data column
 #'
 #' @description
-#' Perform validation on a data column to ensure it contains only numeric values
+#' Perform validation on a data column to ensure it contains only numeric values.
 #'
-#' @param column Column of data to examine. Returns error if column contains
-#' non-numeric value
+#' @param column Column of data to examine. Throws an error if column contains
+#' non-numeric values.
 #'
-#' @return Error code.
-
-library(readr)
+#' @return None. Throws an error if non-numeric values are found.
+#'
+#' @examples
+#' # Should not throw an error
+#' check_numeric_column(c(1, 2, 3.5))
+#'
+#' # Should throw an error
+#' check_numeric_column(c(1, 2, "a"))
 
 check_numeric_column <- function(column) {
-  if (any(is.na(as.numeric(as.character(column))))) {
+  if (is.numeric(column)) {
+    return(TRUE)
+  }
+  numeric_values <- suppressWarnings(as.numeric(as.character(column)))
+  if (any(is.na(numeric_values) & !is.na(column))) {
     stop("Error: Non-numeric values found in the column.")
   }
+  return(TRUE)
 }
